02a5cbcab0c18cf326765715baf44e2b0cf62926
Mikhail Kleymenov <klimggl@gmail.com>
2020-04-12 18:20:39 +0300

    Stub sign in handler implementation

--- packages/voting-poker-api/src/signin/index.ts
+++ packages/voting-poker-api/src/signin/index.ts
@@ -1,10 +1,43 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda';
 
+type SignInRequest = {
+    name: string;
+    sessionId?: string;
+}
+
+type VoterState = {
+    id: string;
+    name: string;
+    voted: boolean;
+    moderator: boolean;
+}
+
+const parse = (event: APIGatewayProxyEvent): SignInRequest => {
+    if (!event.body) {
+        throw new Error('Empty event body');
+    }
+
+    try {
+        return JSON.parse(event.body) as SignInRequest;
+    } catch (error) {
+        throw new Error(`Malformed event body JSON: ${event.body}`);
+    }
+};
+
 export const handler = async (
     event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
+    const request = parse(event);
+
+    const voter: VoterState = {
+        id: event.requestContext.requestId,
+        name: request.name,
+        moderator: true,
+        voted: false,
+    };
+
     return {
-        body: 'OK',
+        body: JSON.stringify(voter),
         statusCode: 200,
     };
 };
