6b5444c7a25a4d21d09de87e628ba2626b028f78
Mikhail Kleymenov <klimggl@gmail.com>
2020-04-26 17:22:24 +0300

    Unit tests

---
+++ packages/voting-poker-api/src/signin/index.test.ts
@@ -0,0 +1,45 @@
+import {handler} from './index';
+
+describe('handler', () => {
+    const MOCK_EVENT = {
+        requestContext: {
+            requestId: '12345',
+        },
+        body: '',
+    };
+
+    it('throws an error if an event has an empty body', async () => {
+        await expect(handler(MOCK_EVENT)).rejects.toThrow('Empty event body');
+    });
+
+    it('throws an error if an event body is not a well-formatted JSON', async () => {
+        const event = {
+            ...MOCK_EVENT,
+            body: '{foo:bar}',
+        };
+
+        await expect(handler(event)).rejects.toThrow(`Malformed event body JSON: ${event.body}`);
+    });
+
+    it('returns a stub voter', async () => {
+        const signInRequest = {
+            name: 'User',
+            sessionId: 'abcd-1234-efgh-5678',
+        };
+
+        const event = {
+            ...MOCK_EVENT,
+            body: JSON.stringify(signInRequest),
+        };
+
+        await expect(handler(event)).resolves.toEqual({
+            body: JSON.stringify({
+                id: event.requestContext.requestId,
+                name: signInRequest.name,
+                moderator: true,
+                voted: false,
+            }),
+            statusCode: 200,
+        });
+    });
+});
